996bfd3686a02e3fdae9510dafba53ecefc44a97
devbugman <cksqhr4961@naver.com>
2023-04-29 23:56:30 +0900

    init project

---
+++ src/main/java/com/newj/NewJApplication.java
@@ -0,0 +1,13 @@
+package com.newj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newj/NewJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
